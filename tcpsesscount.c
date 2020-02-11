bae28d5db4edfbb0bb83efd729baff663c84da79
Ivan <ivanlebeev.90@gmail.com>
2020-02-11 17:58:41 +0300

    Printing all packats from -> to format

--- tcpsesscount.c
+++ tcpsesscount.c
@@ -5,7 +5,8 @@
 //#define _FILENAME "dump_ultra_small.cap"
 
 int main(int argc, char** argv) {
-    int32_t gHeader[6], pHeader[4];
+    const size_t gHeaderSize = 6, pHeaderSize = 4;
+    int32_t gHeader[gHeaderSize], pHeader[pHeaderSize];
     unsigned char *pData;
     if (argc != 2) {
         printf("Usage: tcpsesscount filename\n");
@@ -18,17 +19,28 @@ int main(int argc, char** argv) {
         return 0;
     }
     
-    for (size_t i = 0, gHeaderSize = fread(gHeader, sizeof(gHeader[0]), 6, dump); i < gHeaderSize; ++i) {
+    fread(gHeader, sizeof(gHeader[0]), gHeaderSize, dump);
+    for (size_t i = 0; i < gHeaderSize; ++i) {
         printf("%X ", gHeader[i]);
     }
     printf("\n");
     int c = 0;
-    while (fread(pHeader, sizeof(pHeader[0]), 4, dump)) {
-        pData = (unsigned char*) malloc(pHeader[2] * sizeof(unsigned char));
+    while (fread(pHeader, sizeof(pHeader[0]), pHeaderSize, dump)) {
+        pData = (unsigned char*) malloc(pHeader[2] * sizeof(unsigned char)); // Might be faster than realloc(pData, pHeader[2])
         fread(pData, sizeof(pData[0]), pHeader[2], dump);
+        printf("%i. ", 1 + c);
+        for (size_t i = 6; i < 12; ++i) {
+            printf("%x", pData[i]);
+            printf(i - 11 ? ":" : " ");
+        }
+        printf("-> ");
+        for (size_t i = 0; i < 6; ++i) {
+            printf("%x", pData[i]);
+            printf(i - 5 ? ":" : " ");
+        }
+        printf("\n");
         c++;
         free(pData);
     }
-    printf("Number of packets: %d\n", c);
     return 0;
 }
