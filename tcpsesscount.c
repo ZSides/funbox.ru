39b9ed3f8bfea6571a6cb9025c24d6b65f5dba3c
Ivan <ivanlebeev.90@gmail.com>
2020-02-13 00:06:43 +0300

    only tcp packets will be parsed

--- tcpsesscount.c
+++ tcpsesscount.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #define _FILENAME "rb919.cap"
 //#define _FILENAME "dump_ultra_small.cap"
 
 typedef struct {
     unsigned char mac_src[6], mac_dst[6];
     u_int16_t port_src, port_dst;
+    char ipv;
 } tcpPacketData;
 
-tcpPacketData parseTCP(unsigned char *d) {
+tcpPacketData parseTCP(unsigned char *d, const u_int16_t *etype) {
     tcpPacketData tcp;
     for (size_t i = 6; i < 12; ++i) {
         tcp.mac_src[i - 6] = d[i];
@@ -19,12 +21,24 @@ tcpPacketData parseTCP(unsigned char *d) {
     }
     tcp.port_src = (d[34] << 8) + d[35];
     tcp.port_dst = (d[36] << 8) + d[37];
+    tcp.ipv = 4 + 2 * (*etype != 0x0800);
     return tcp;
+}
+
+void printInfo(const tcpPacketData *tp) {
+    printf("IPv%i. ", tp->ipv);
+    for(int i = 0; i < 6; ++i)
+        printf("%X ", tp->mac_src[i]);
+    printf(": %i -> ", tp->port_src);
+    for(int i = 0; i < 6; ++i)
+        printf("%X ", tp->mac_dst[i]);
+    printf(": %i\n", tp->port_dst);
 }
 
 int main(int argc, char** argv) {
     const size_t gHeaderSize = 6, pHeaderSize = 4;
     int32_t gHeader[gHeaderSize], pHeader[pHeaderSize];
+    u_int16_t eth_type;
     unsigned char *pData;
     tcpPacketData tcpPacket;
     if (argc != 2) {
@@ -46,13 +60,11 @@ int main(int argc, char** argv) {
     while (fread(pHeader, sizeof(pHeader[0]), pHeaderSize, dump)) {
         pData = (unsigned char*) malloc(pHeader[2] * sizeof(unsigned char)); // Might be faster than realloc(pData, pHeader[2])
         fread(pData, sizeof(pData[0]), pHeader[2], dump);
-        tcpPacket = parseTCP(pData);
-        for(int i = 0; i < 6; ++i)
-            printf("%X ", tcpPacket.mac_src[i]);
-        printf(": %i -> ", tcpPacket.port_src);
-        for(int i = 0; i < 6; ++i)
-            printf("%X ", tcpPacket.mac_dst[i]);
-        printf(": %i\n", tcpPacket.port_dst);
+        eth_type = (pData[12] << 8) + pData[13];
+        if ((pData[23] == 6) && (eth_type == 0x0800 || eth_type == 0x86DD)) {
+            tcpPacket = parseTCP(pData, &eth_type);
+            printInfo(&tcpPacket);
+        }
         free(pData);
     }
     return 0;
